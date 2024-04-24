02626c1fcf995356853361b7f9c67f2fa42a5259
mandla137 <62339827+mandla137@users.noreply.github.com>
2024-04-24 21:33:49 +0200

    Add files via upload

---
+++ src/java/com/ajax/ComposerData.java
@@ -0,0 +1,75 @@
+package com.ajax;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author nbuser
+ */
+public class ComposerData {
+
+    private final HashMap composers = new HashMap();
+
+    public HashMap getComposers() {
+        return composers;
+    }
+
+    public ComposerData() {
+
+        composers.put("1", new Composer("1", "Ts'epo Mofolo", "Ntholi Nkhato", "Itumeleng Mokhachane"));
+        composers.put("2", new Composer("2", "Diana Moopisa", "Mohale Tlali", "Thokoana Thokoana"));
+        composers.put("3", new Composer("3", "Manapo Sekopo", "Mpho Takalimane", "Mpho Ranyali"));
+        composers.put("4", new Composer("4", "Molemo Borotho", "Khauta Rantai", "Mandla Ndabambi"));
+        composers.put("5", new Composer("5", "Katleho Toolo", "Molise Nts'ihlele", "Motumi Lemphane"));
+        composers.put("6", new Composer("6", "Neo Shakhane", "Relebohile Mohapi", "Mpho Ramokotjo"));
+        composers.put("7", new Composer("7", "Lebohang Raboroko", "Mejaele Mejaele", "Lerato Tohlang"));
+
+        composers.put("8", new Composer("8", "Lebohang Raboroko", "Mejaele Mejaele", "Lerato Tohlang"));
+        composers.put("9", new Composer("9", "Katleho Toolo", "Molise Nts'ihlele", "Motumi Lemphane"));
+        composers.put("10", new Composer("10", "Manapo Sekopo", "Mpho Takalimane", "Mpho Ranyali"));
+        composers.put("11", new Composer("11", "Diana Moopisa", "Mohale Tlali", "Thokoana Thokoana"));
+        composers.put("12", new Composer("12", "Ts'epo Mofolo", "Ntholi Nkhato", "Itumeleng Mokhachane"));
+        composers.put("13", new Composer("13", "Lebohang Raboroko", "Mejaele Mejaele", "Lerato Tohlang"));
+        composers.put("14", new Composer("14", "Gustav", "Mahler", "Classical"));
+        composers.put("15", new Composer("15", "Wolfgang Amadeus", "Mozart", "Classical"));
+        composers.put("16", new Composer("16", "Johann", "Pachelbel", "Classical"));
+        composers.put("17", new Composer("17", "Gioachino", "Rossini", "Classical"));
+        composers.put("18", new Composer("18", "Dmitry", "Shostakovich", "Classical"));
+        composers.put("19", new Composer("19", "Richard", "Wagner", "Classical"));
+
+        composers.put("20", new Composer("20", "Louis-Hector", "Berlioz", "Romantic"));
+        composers.put("21", new Composer("21", "Georges", "Bizet", "Romantic"));
+        composers.put("22", new Composer("22", "Cesar", "Cui", "Romantic"));
+        composers.put("23", new Composer("23", "Claude", "Debussy", "Romantic"));
+        composers.put("24", new Composer("24", "Edward", "Elgar", "Romantic"));
+        composers.put("25", new Composer("25", "Gabriel", "Faure", "Romantic"));
+        composers.put("26", new Composer("26", "Cesar", "Franck", "Romantic"));
+        composers.put("27", new Composer("27", "Edvard", "Grieg", "Romantic"));
+        composers.put("28", new Composer("28", "Nikolay", "Rimsky-Korsakov", "Romantic"));
+        composers.put("29", new Composer("29", "Franz Joseph", "Liszt", "Romantic"));
+
+        composers.put("30", new Composer("30", "Felix", "Mendelssohn", "Romantic"));
+        composers.put("31", new Composer("31", "Giacomo", "Puccini", "Romantic"));
+        composers.put("32", new Composer("32", "Sergei", "Rachmaninoff", "Romantic"));
+        composers.put("33", new Composer("33", "Camille", "Saint-Saens", "Romantic"));
+        composers.put("34", new Composer("34", "Franz", "Schubert", "Romantic"));
+        composers.put("35", new Composer("35", "Robert", "Schumann", "Romantic"));
+        composers.put("36", new Composer("36", "Jean", "Sibelius", "Romantic"));
+        composers.put("37", new Composer("37", "Bedrich", "Smetana", "Romantic"));
+        composers.put("38", new Composer("38", "Richard", "Strauss", "Romantic"));
+        composers.put("39", new Composer("39", "Pyotr Il'yich", "Tchaikovsky", "Romantic"));
+        composers.put("40", new Composer("40", "Guiseppe", "Verdi", "Romantic"));
+
+        composers.put("41", new Composer("41", "Bela", "Bartok", "Post-Romantic"));
+        composers.put("42", new Composer("42", "Leonard", "Bernstein", "Post-Romantic"));
+        composers.put("43", new Composer("43", "Benjamin", "Britten", "Post-Romantic"));
+        composers.put("44", new Composer("44", "John", "Cage", "Post-Romantic"));
+        composers.put("45", new Composer("45", "Aaron", "Copland", "Post-Romantic"));
+        composers.put("46", new Composer("46", "George", "Gershwin", "Post-Romantic"));
+        composers.put("47", new Composer("47", "Sergey", "Prokofiev", "Post-Romantic"));
+        composers.put("48", new Composer("48", "Maurice", "Ravel", "Post-Romantic"));
+        composers.put("49", new Composer("49", "Igor", "Stravinsky", "Post-Romantic"));
+        composers.put("50", new Composer("50", "Carl", "Orff", "Post-Romantic"));
+
+    }
+}
